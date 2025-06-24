50b4484aca0f662543be3a8f788c23b9a8a34944
ilhamutomo24 <ilhamutomo24@gmail.com>
2025-06-24 14:38:05 +0700

    add: models

--- models/project.go
+++ models/project.go
@@ -1,26 +1,29 @@
 package models
 
-import "time"
+import (
+	"gorm.io/datatypes"
+	"time"
+)
 
 type Project struct {
-	ID            uint       `gorm:"primaryKey" json:"id"`
-	Logo          string     `json:"logo"`
-	Owner         string     `json:"owner"`
-	Title         string     `json:"title"`
-	Company       string     `json:"company"`
-	StartDate     *time.Time `json:"start_date"`
-	EndDate       *time.Time `json:"end_date"`
-	Types         string     `json:"types"`
-	Duration      string     `json:"duration"`      // "day" | "month" | "year"
-	Participation string     `json:"participation"` // "remote" | "onsite" | "hybrid"
-	Approval      string     `json:"approval"`      // "yes" | "no"
-	Description   string     `json:"description"`
-	Salary        float64    `json:"salary"`
-	Priority      string     `json:"priority"`
-	Status        string     `json:"status"`
-	CreatedAt     time.Time  `json:"created_at"`
-	UpdatedAt     time.Time  `json:"updated_at"`
-	Address       Address    `gorm:"foreignKey:ProjectID"`
-	Members       []Member   `gorm:"foreignKey:ProjectID"`
-	Tags          []Tag      `gorm:"many2many:project_tags"`
+	ID            uint           `gorm:"primaryKey" json:"id"`
+	Logo          string         `json:"logo"`
+	Owner         string         `gorm:"size:50" json:"owner"`
+	Title         string         `gorm:"size:100" json:"title"`
+	Company       string         `gorm:"size:100" json:"company"`
+	StartDate     *time.Time     `json:"start_date"`
+	EndDate       *time.Time     `json:"end_date"`
+	Types         string         `gorm:"size:10" json:"types"`
+	Duration      string         `gorm:"size:10" json:"duration"`
+	Participation string         `gorm:"size:10" json:"participation"`
+	Address       datatypes.JSON `json:"address"` // format: struct => json.Marshal
+	Approval      string         `gorm:"size:5" json:"approval"`
+	Description   string         `json:"description"`
+	Tags          datatypes.JSON `json:"tags"` // format: array struct => json.Marshal
+	Salary        float64        `gorm:"type:numeric(15,2)" json:"salary"`
+	Priority      string         `gorm:"size:10" json:"priority"`
+	IsPrivate     bool           `json:"is_private"`
+	IsClosed      bool           `json:"is_closed"`
+	CreatedAt     time.Time      `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt     time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
 }
